7d9295ffb546967113550dba019115cbb185fad5
Jay K <33371449+jay94ks@users.noreply.github.com>
2019-11-13 22:17:50 +0900

    Removal of IAllocator

--- src/boxpp/impls/memory/Boilerplate.hpp
+++ src/boxpp/impls/memory/Boilerplate.hpp
@@ -2,12 +2,10 @@
 #define __BOXPP_BOILERPLATE_HPP__
 
 #include <boxpp/IBox.hpp>
-#include <boxpp/memory/Allocator.hpp>
 
 namespace boxpp {
 
 	namespace boilerplates {
-		struct SAllocInfo { IAllocator* Allocator; };
 	}
 }
 #endif // !__BOXPP_BOILERPLATE_HPP__
